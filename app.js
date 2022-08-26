07bf3c78447bd7b8b66e2855d76026680dd8845e
Javier Quiroga <javierquirogae@gmail.com>
2022-08-25 21:29:05 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const form = document.querySelector("form");
 const todoList = document.querySelector("#todo_list");
-
+let UI = parseInt(localStorage.getItem("UI")) || 0;
 const savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
 
 for (let i = 0; i < savedTodos.length; i++) {
   let newLi = document.createElement("li");
   let newButton = document.createElement("button");
   newLi.innerText = savedTodos[i].task;
+  newLi.id = savedTodos[i].ID;;
   newLi.isCompleted = savedTodos[i].isCompleted ? true : false;
   if (newLi.isCompleted) {
-    newLi.style.textDecoration = "line-through";
+    newLi.className = "completed";
   }
-  newLi.className = "todo";
+  else{
+    newLi.className = "todo";
+  }
+  
   newButton.innerText = "";
   newLi.append(newButton);
   todoList.appendChild(newLi);
@@ -20,29 +24,53 @@ for (let i = 0; i < savedTodos.length; i++) {
 
 form.addEventListener("submit", function(event) {
     event.preventDefault();
+    UI++;
     const newTodoInput = document.querySelector("#new-todo");
     const newLi = document.createElement("li");
     const newButton = document.createElement("button");
     newLi.innerText = newTodoInput.value;
     newLi.className = "todo";
+    newLi.id = UI;
     newButton.innerText = "";
     newLi.append(newButton);
     todoList.append(newLi);
-    savedTodos.push({ task: newLi.innerText, isCompleted: false });
+    savedTodos.push({ ID: UI, task: newLi.innerText, isCompleted: false });
+    localStorage.setItem("UI", UI);
     localStorage.setItem("todos", JSON.stringify(savedTodos));
     form.reset();
+    console.log(UI);
   });
 
   todoList.addEventListener("click", function(event) {
     if (event.target.tagName === "BUTTON") {
       event.target.parentElement.remove();
+      let this_id = event.target.parentElement.id;
+      let str_id = this_id.toString();
+      let removeIndex = savedTodos.findIndex(x => x.ID == str_id);
+      console.log(removeIndex);
+      if (removeIndex > -1) { 
+        savedTodos.splice(removeIndex, 1); 
+      }
+      let UI = parseInt(localStorage.getItem("UI"))
       localStorage.clear();
+      localStorage.setItem("todos", JSON.stringify(savedTodos));
+      localStorage.setItem("UI", UI);
     }
   });
 
   todoList.addEventListener("click", function(event) {
     if (event.target.textContent) {
       event.target.className = "completed";
+      let this_id = event.target.id;
+      let str_id = this_id.toString();
+      let crossIndex = savedTodos.findIndex(x => x.ID == str_id);
+      //obj = savedTodos[crossIndex];
+      savedTodos[crossIndex].isCompleted = true;
+      //console.log(obj);
+      let UI = parseInt(localStorage.getItem("UI"))
+      localStorage.clear();
+      localStorage.setItem("todos", JSON.stringify(savedTodos));
+      localStorage.setItem("UI", UI);
     }
   });
 
